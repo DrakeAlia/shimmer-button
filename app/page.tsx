04e29ed2db86fb44f1fe52e0dea50bb9c5fb1b55
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-12-13 11:37:37 -0800

    added some class names to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Image from 'next/image'
+
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-start">
+      <div className="mt-10 flex items-center justify-center gap-x-6">
+        <div className="mt-10 flex items-center justify-center gap-x-6">
       <h1>Setup!</h1>
+          <Link
+            href="#"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
+          >
+            Get started
+          </Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
