6c4b361e9314e20063f0640de3a914bae672ae11
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-12-13 18:28:31 -0800

    added more button styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import { buttonVariants } from "../components/ui/button";
 
 export default function Home() {
   return (
-    <div className="bg-black flex flex-col text-center items-center mx-auto py-20 max-w-3x">
+    // Background
+    <div className="bg-black flex flex-col text-center items-center min-h-screen mx-auto py-20 max-w-3x">
       <h1 className="animate-bounce text-4xl text-gray-200 font-bold sm:text-6xl">
         Buttons!
       </h1>
-      {/* < className="flex flex-col sm:flex-row gap-4 mt-10 bg-yellow-400"> */}
+      {/* <div className="flex flex-col sm:flex-row gap-4 mt-10 bg-yellow-400"> */}
+      {/* Button Body */}
       <div className="flex flex-col gap-4 mt-40">
         <Link
           href="#"
@@ -17,7 +19,7 @@ export default function Home() {
         </Link>
         <Link
           href="#"
-          className="rounded-md animate-pulse bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md animate-pulse bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Button Style 2
         </Link>
@@ -27,14 +29,28 @@ export default function Home() {
         >
           Button Style 3
         </Link>
+        {/* Shadcn Button: */}
         <Link href="#" className={buttonVariants()}>
-          Button Style Shadcn
+          Button Style 4
         </Link>
+        {/* Gradient Button Effect: */}
+        {/* Button */}
+        <button className="relative overflow-hidden rounded-lg px-20 py-6">
+          {/* Button Body */}
+          <span className="absolute inset-px z-10 flex items-center justify-center rounded-lg bg-black bg-gradient-to-t from-neutral-800 text-neutral-300">
+            Button Style 5
+          </span>
+          {/* Animated Border */}
+          <span
+            aria-hidden
+            className="absolute inset-0 z-0 scale-x-[2.0] blur before:absolute before:inset-0 before:top-1/2 before:aspect-square before:animate-disco before:bg-gradient-conic before:from-purple-700 before:via-red-500 before:to-amber-400"
+          />
+        </button>
       </div>
-      <div className="flex flex-col gap-4 mt-10">
-        {/* shimmer button example */}
-        {/* <div className="not-prose max-w-screen relative flex flex-col items-center justify-center rounded-xl border bg-background p-8 md:p-16"> */}
-          <div className="absolute inset-0 h-full w-full bg-[radial-gradient(#00000055_1px,transparent_1px)] dark:bg-[radial-gradient(#ffffff22_1px,transparent_1px)] lab-bg [background-size:16px_16px]"></div>
+      {/* <div className="flex flex-col gap-4 mt-10"> */}
+      {/* Shimmer Button: */}
+      {/* <div className="not-prose max-w-screen relative flex flex-col items-center justify-center rounded-xl border bg-background p-8 md:p-16"> */}
+      {/* <div className="absolute inset-0 h-full w-full bg-[radial-gradient(#00000055_1px,transparent_1px)] dark:bg-[radial-gradient(#ffffff22_1px,transparent_1px)] lab-bg [background-size:16px_16px]"></div>
           <button className="group relative z-0 flex cursor-pointer items-center justify-center overflow-hidden whitespace-nowrap border border-white/10 px-6 py-3 text-white [background:var(--bg)] [border-radius:var(--radius)] dark:text-black transform-gpu transition-transform duration-300 ease-in-out active:translate-y-[1px] shadow-2xl">
             <div className="-z-30 blur-[2px] absolute inset-0 overflow-visible [container-type:size]">
               <div className="absolute inset-0 h-[100cqh] animate-slide [aspect-ratio:1] [border-radius:0] [mask:none]">
@@ -47,8 +63,8 @@ export default function Home() {
             <div className="insert-0 absolute h-full w-full rounded-2xl px-4 py-1.5 text-sm font-medium shadow-[inset_0_-8px_10px_#ffffff1f] transform-gpu transition-all duration-300 ease-in-out group-hover:shadow-[inset_0_-6px_10px_#ffffff3f] group-active:shadow-[inset_0_-10px_10px_#ffffff3f]"></div>
             <div className="absolute -z-20 [background:var(--bg)] [border-radius:var(--radius)] [inset:var(--cut)]"></div>
           </button>
-        </div>
-      </div>
-    // </div>
+        </div> */}
+      {/* </div> */}
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage: {
+        "gradient-conic": "conic-gradient(var(--tw-gradient-stops))",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -65,12 +68,17 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        disco: {
+          "0%": { transform: "translateY(-50%) rotate(0deg)" },
+          "100%": { transform: "translateY(-50%) rotate(360deg)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        disco: "disco 1.5s linear infinite",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
